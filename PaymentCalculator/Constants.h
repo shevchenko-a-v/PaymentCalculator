04db6f5cd8676ca1451eabdd2210d45ac13d4472
shevchenko-a-v <38935246+shevchenko-a-v@users.noreply.github.com>
2018-06-22 17:42:14 +0300

    First step

---
+++ PaymentCalculator/Constants.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace PaymentCalculator
+{
+	namespace Constants
+	{
+		const std::string AMOUNT_LINE_START = "amount";
+		const std::string INTEREST_LINE_START = "interest";
+		const std::string DOWNPAYMENT_LINE_START = "downpayment";
+		const std::string TERM_LINE_START = "term";
+	}
+}
---
+++ PaymentCalculator/CreditInfo.h
@@ -0,0 +1,32 @@
+#pragma once
+
+namespace PaymentCalculator
+{
+	class CreditInfo
+	{
+	public:
+		// Initializes CreditInfo instance with credit parameters:
+		// amount [$]
+		// interest [%]
+		// downpayment [$]
+		// term [years]
+		CreditInfo(double amount, double interest, double downpayment, double term);
+
+		inline double GetMounthlyPayment() { return mounthlyPayment; }
+		inline double GetTotalInterest() { return totalInterest; }
+		inline double GetTotalPayment() { return totalPayment; }
+
+	private:
+		void InitializeCalculatedFields();
+
+	private:
+		double amount;
+		double interest;
+		double downpayment;
+		double term;
+		// calculable fields
+		double mounthlyPayment;
+		double totalInterest;
+		double totalPayment;
+	};
+}
